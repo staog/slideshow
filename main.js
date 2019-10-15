e5af5eb215f6145b1f52ccec96fa327c26b715cb
Milan V. Kecojević <staog@gmx.com>
2019-10-15 21:43:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 var slide = document.getElementsByTagName('img')[1];
 var text = document.getElementsByTagName('h2')[0];
-var images = ['images/alesund.jpg', 'images/grandturk.jpg', 'images/mazatlan.jpg',
- 'images/puertovallarta.jpg', 'images/princesscays.jpg'];
+var images = ['images/DSCN3703.JPG', 'images/DSCN3704.JPG', 'images/DSCN3706.JPG',
+ 'images/DSCN3707.JPG', 'images/DSCN3708.JPG', 'images/DSCN3709.JPG', 'images/DSCN3712.JPG', 'images/DSCN3713.JPG', 'images/DSCN3715.JPG'];
 var slider = setInterval(slideShow, 3000);
 var sliderActive = true;
 var timer = 0;
